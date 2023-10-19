56821cee0b47c79ae61a6d735dc619854ce3f02c
FCamposE <138427832+FCamposE@users.noreply.github.com>
2023-10-19 18:16:26 -0500

    cambios
    menu hamburguesa y 2 vistas mas

---
+++ app/src/main/java/com/proyecto/android/savemymoney/AgregarIngresoActivity.kt
@@ -0,0 +1,11 @@
+package com.proyecto.android.savemymoney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgregarIngresoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_ingreso)
+    }
+}
--- app/src/main/java/com/proyecto/android/savemymoney/PrincipalActivity.kt
+++ app/src/main/java/com/proyecto/android/savemymoney/PrincipalActivity.kt
@@ -3,17 +3,23 @@ package com.proyecto.android.savemymoney
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 import com.proyecto.android.savemymoney.adapter.GastoAdapter
 import com.proyecto.android.savemymoney.modelo.Gasto
 import com.proyecto.android.savemymoney.modelo.Usuario
 
 
-class PrincipalActivity : AppCompatActivity() {
+class PrincipalActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var tvUsuario: TextView
 
@@ -22,9 +28,22 @@ class PrincipalActivity : AppCompatActivity() {
     private lateinit var adapterGasto: GastoAdapter
     private lateinit var layoutManager: LinearLayoutManager
     private lateinit var fabAgregarGasto: FloatingActionButton
+
+    private lateinit var drawerPrincipal: DrawerLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var nvPrincipal: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
+        this.drawerPrincipal = findViewById(R.id.dlPrincipal)
+        this.toolbar = findViewById(R.id.toolbar2)
+        this.nvPrincipal = findViewById(R.id.nvPrincipal)
+
+        setSupportActionBar(this.toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        this.setNavigationDrawer()
 
         if(intent.getSerializableExtra("listaGastosAgregar") != null)
             this.gastos = intent.getSerializableExtra("listaGastosAgregar") as ArrayList<Gasto>
@@ -36,6 +55,32 @@ class PrincipalActivity : AppCompatActivity() {
         this.tvUsuario = findViewById(R.id.tvUsuario)
         tvUsuario.text = "Bienvenido, " + usu?.nombre*/
 
+    }
+    private fun setNavigationDrawer(){
+        val toogle = ActionBarDrawerToggle(this,
+            this.drawerPrincipal,
+            this.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        toogle.isDrawerIndicatorEnabled = true
+        this.drawerPrincipal.addDrawerListener(toogle)
+        toogle.syncState()
+        this.nvPrincipal.setNavigationItemSelectedListener(this)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_agregar_gasto -> {
+                val intent = Intent(this, AgregarGastoActivity::class.java)
+                intent.putParcelableArrayListExtra("listaGastosPrincipal", this.gastos)
+                startActivity(intent)
+            }
+            R.id.nav_agregar_ingreso -> {val intent = Intent(this, AgregarIngresoActivity::class.java)
+            startActivity(intent)}
+            R.id.nav_dashboard -> {val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)}
+            R.id.nav_salir -> finish()
+        }
+        item.isChecked = true
+        drawerPrincipal.closeDrawer(GravityCompat.START)
+        return true
     }
 
     private fun iniciarComponentes(){
